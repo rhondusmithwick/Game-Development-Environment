8961ff03d161b05815d4003b7cc4b890fecd8f9e
Melissa Zhang <mfz2@duke.edu>
2016-04-03 23:29:16 -0400

    editor super class again

--- src/api/Editor.java
+++ src/api/Editor.java
@@ -3,19 +3,27 @@ package api;
 import java.util.ArrayList;
 import java.util.List;
 
-import model.entity.Entity;
+import model.entity.EntitySystem;
 
 public abstract class Editor implements IEditor{
-	private List<Entity> entityList;
+	private List<IEntity> entityList;
+	
 	public Editor(){
-		entityList = new ArrayList<Entity>();
+		entityList = new ArrayList<IEntity>();
 	}
-	public List<Entity> getEntities(){
+	public List<IEntity> getEntities(){
 		return entityList;
 		
 	}
-	public void addEntity(Entity entity){
+	public void addEntity(IEntity entity){
 		entityList.add(entity);
 	}
 	
+	public void createEntity(){
+		EntitySystem entitySystem = new EntitySystem();
+		IEntity entity = entitySystem.createEntity();
+		addEntity(entity);
+	}
+	
+	public abstract void updateEditor();
 }
--- src/api/IEditor.java
+++ src/api/IEditor.java
@@ -1,12 +1,10 @@
 package api;
 
 import javafx.scene.Group;
-import javafx.scene.control.Button;
 import javafx.scene.layout.Pane;
 
 public interface IEditor { //This will be an abstract class that all editors will extend.
 	
-
     void loadDefaults();
 
     Group getGroup(); // This is a method all subclasses should implement.
