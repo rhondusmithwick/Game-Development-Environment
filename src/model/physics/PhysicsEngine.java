e4656e46b9a07029246328b1579762f9d8191aab
Tom Wu <cw223@duke.edu>
2016-04-05 22:21:03 -0400

    worked on physics

--- src/model/physics/PhysicsEngine.java
+++ src/model/physics/PhysicsEngine.java
@@ -1,78 +1,78 @@
 package model.physics;
 
-import model.entity.IEntity;
-import model.entity.IEntitySystem;
-import api.IPhysicsEngine;
-import javafx.scene.shape.Shape;
-import model.component.movement.Position;
-import model.component.movement.Velocity;
-import model.component.physics.Collision;
-
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+import com.google.common.collect.Sets;
+
+import api.IPhysicsEngine;
+import javafx.scene.shape.Shape;
+import model.component.movement.Position;
+import model.component.movement.Velocity;
+import model.component.physics.Collision;
+import model.entity.IEntity;
+import model.entity.IEntitySystem;
 
 public class PhysicsEngine implements IPhysicsEngine {
-    IEntitySystem settings;
+	IEntitySystem settings;
 
-    public PhysicsEngine(IEntitySystem settings) {
-        this.settings = settings;
-    }
+	public PhysicsEngine(IEntitySystem settings) {
+		this.settings = settings;
+	}
 
-    @Override
-    public IEntitySystem update(IEntitySystem universe, double dt) {
-        Collection<IEntity> dynamicEntities = universe.getAllEntities().stream()
-                .filter(p -> p.hasComponents(Position.class, Velocity.class))
-                .collect(Collectors.toSet());
-        dynamicEntities.stream().forEach(p -> {
-            Position pos = p.getComponent(Position.class);
-            Velocity velocity = p.getComponent(Velocity.class);
-            double dx = dt * velocity.getVX();
-            double dy = dt * velocity.getVY();
-            pos.add(dx, dy);
-        });
-        return universe;
-    }
+	@Override
+	public IEntitySystem update(IEntitySystem universe, double dt) {
+		Collection<IEntity> dynamicEntities = universe.getAllEntities().stream()
+				.filter(p -> p.hasComponents(Position.class, Velocity.class)).collect(Collectors.toSet());
+		dynamicEntities.stream().forEach(p -> {
+			Position pos = p.getComponent(Position.class);
+			Velocity velocity = p.getComponent(Velocity.class);
+			double dx = dt * velocity.getVX();
+			double dy = dt * velocity.getVY();
+			pos.add(dx, dy);
+		});
+		return universe;
+	}
 
-    public void applyImpulse(IEntity body, Impulse J) {
-        if (body.hasComponent(Velocity.class)) {
-            Velocity v = body.getComponent(Velocity.class);
-            v.add(J.getJx(), J.getJy());
-        }
-    }
+	public void applyImpulse(IEntity body, Impulse J) {
+		if (body.hasComponent(Velocity.class)) {
+			Velocity v = body.getComponent(Velocity.class);
+			v.add(J.getJx(), J.getJy());
+		}
+	}
 
-    public boolean areCollidingEntities(IEntity e1, IEntity e2) {
-        List<Collision> cList1 = e1.getComponentList(Collision.class);
-        List<Collision> cList2 = e2.getComponentList(Collision.class);
-        for (Collision c1 : cList1) {
-            Shape mask1 = c1.getMask();
-            Collection<String> IDList1 = c1.getIDs();
-            for (Collision c2 : cList2) {
-                Shape mask2 = c2.getMask();
-                Collection<String> IDList2 = c2.getIDs();
-                if (!this.areIntersectingIDLists(IDList1, IDList2)) {
-                    // TODO
-                }
-            }
-        }
+	// might be useless:
+	public boolean areCollidingEntities(IEntity e1, IEntity e2) {
+		List<Collision> cList1 = e1.getComponentList(Collision.class);
+		List<Collision> cList2 = e2.getComponentList(Collision.class);
+		for (Collision c1 : cList1) {
+			Shape mask1 = c1.getMask();
+			Collection<String> IDList1 = c1.getIDs();
+			for (Collision c2 : cList2) {
+				Shape mask2 = c2.getMask();
+				Collection<String> IDList2 = c2.getIDs();
+				if (!this.areIntersectingIDLists(IDList1, IDList2)) {
+					// TODO
+				}
+			}
+		}
 
-        return false; // TODO
-    }
+		return false; // TODO
+	}
 
-    public Collection<IEntity> getEntitiesCollidingWith (IEntity e) {
+	// probably more useful:
+	public Collection<IEntity> getEntitiesCollidingWith(IEntity e) {
 
-        return null; // TODO
-    }
+		return null; // TODO
+	}
 
-    private boolean areIntersectingIDLists (Collection<String> IDList1,
-                                            Collection<String> IDList2) {
-        Set<String> IDSet = new HashSet<String>();
-        IDSet.addAll(IDList1);
-        IDSet.addAll(IDList2);
-        return (IDSet.size() < IDList1.size() + IDList2.size());
-    }
+	private boolean areIntersectingIDLists(Collection<String> IDList1, Collection<String> IDList2) {
+		Set<String> s1 = new HashSet<String>(IDList1);
+		Set<String> s2 = new HashSet<String>(IDList2);
+		return (Sets.intersection(s1, s2).size() > 0);
+	}
 
 }
