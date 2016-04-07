868433432148c50e0a0702834cfaba390a31764f
calisnelson <can18@duke.edu>
2016-04-06 22:28:53 -0400

    comment out lines

--- src/usecases/UseCase17.java
+++ src/usecases/UseCase17.java
@@ -1,18 +1,18 @@
 package usecases;
 
-import api.EditorEntity;
+//import api.EditorEntity;
 
 public class UseCase17 {
 
     // game creator adds power up
     void doUseCase() {
-        EditorEntity myEditor = new EntityEditor();
-        myEditor.show();
+        //EditorEntity myEditor = new EntityEditor();
+        //myEditor.show();
         //waiting for user to enter data and press create button
         //once create button is pressed
-        String filename = null; //file name for current game
-        myEditor.writeToFile(filename);
-        myEditor.close();
+        //String filename = null; //file name for current game
+        //myEditor.writeToFile(filename);
+        //myEditor.close();
     }
 
 
