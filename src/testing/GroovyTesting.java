a06ed3d11a4378687528fe1800c5a389876dc32c
Rhondu Smithwick <rhondusmithwick@gmail.com>
2016-04-07 23:57:37 -0400

    Some more groovy testing!

--- src/testing/GroovyTesting.java
+++ src/testing/GroovyTesting.java
@@ -5,6 +5,8 @@ import model.component.movement.Position;
 import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
 import javax.script.ScriptException;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 
 /**
  * Created by rhondusmithwick on 4/7/16.
@@ -22,16 +24,17 @@ public class GroovyTesting implements Tester {
     @Override
     public void test() {
         Position position1 = new Position(80, 500);
+        engine.put("position1", position1);
+        new Thread(this::run).start();
+        Position position = (Position) engine.get("position");
+        System.out.println("Created by groovy position" + position);
+        System.out.println("Created by Java Position: " + position1);
+    }
+
+    private void run() {
         try {
-            engine.put("position1", position1);
-            engine.eval("import model.component.movement.Position; position = new Position(50, 50)");
-            engine.eval("position.setX(100)");
-            engine.eval("position.setY(5000)");
-            engine.eval("position1.setX(888888)");
-            Position position = (Position) engine.get("position");
-            System.out.println("Created by groovy position" + position);
-            System.out.println("Created by Java Position: " + position1);
-        } catch (ScriptException e) {
+            engine.eval(new FileReader("resources/groovyScripts/PositionTest.groovy"));
+        } catch (ScriptException | FileNotFoundException e) {
             e.printStackTrace();
         }
     }
