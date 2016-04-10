c406a636a6c1f15f54726a16165e0df7c0bc9851
Tom Wu <cw223@duke.edu>
2016-04-10 16:27:36 -0400

    deprecated old event classes/interfaces

--- src/api/IActiveComponent.java
+++ src/api/IActiveComponent.java
@@ -1,15 +1,17 @@
 package api;
 
+@Deprecated
 /**
  * The interface for an active component.
  *
  * @author Tom Wu
  */
 public interface IActiveComponent extends IComponent {
-    /**
-     * Can act on anything in the system
-     *
-     * @param system might need to be restricted later!
-     */
-    void act(ISystem system);
+	/**
+	 * Can act on anything in the system
+	 *
+	 * @param system
+	 *            might need to be restricted later!
+	 */
+	void act(ISystem system);
 }
--- src/testing/InputSystemTesting.java
+++ src/testing/InputSystemTesting.java
@@ -1,8 +1,12 @@
 package testing;
 
-import api.IEntity;
+import java.io.File;
+import java.io.IOException;
+
 import com.google.common.base.Charsets;
 import com.google.common.io.Files;
+
+import api.IEntity;
 import events.Action;
 import events.InputSystem;
 import javafx.application.Application;
@@ -14,9 +18,6 @@ import model.component.movement.Position;
 import model.entity.Entity;
 import model.entity.EntitySystem;
 
-import java.io.File;
-import java.io.IOException;
-
 /**
  * Created by rhondusmithwick on 4/10/16.
  *
@@ -24,41 +25,45 @@ import java.io.IOException;
  */
 public class InputSystemTesting extends Application {
 
-    private final EntitySystem universe = new EntitySystem();
-    private final InputSystem inputSystem = new InputSystem();
+	private final EntitySystem universe = new EntitySystem();
+	private final InputSystem inputSystem = new InputSystem();
 
-    private final String MOVE_SCRIPT = "resources/groovyScripts/SignalScript.groovy";
+	private final String MOVE_SCRIPT = "resources/groovyScripts/SignalScript.groovy";
 
-    private Scene mYInit() {
-        BorderPane splash = new BorderPane();
-        Scene scene = new Scene(splash, 500, 500);
-        scene.setOnKeyPressed(e -> inputSystem.take(e.getCode(), universe));
-        return scene;
-    }
+	private Scene mYInit() {
+		BorderPane splash = new BorderPane();
+		Scene scene = new Scene(splash, 500, 500);
+		scene.setOnKeyPressed(e -> inputSystem.take(e.getCode(), universe));
+		return scene;
+	}
 
-    private void setUp() {
-        IEntity entity = new Entity("Ben");
-        Position position = new Position();
-        entity.forceAddComponent(position, true);
-        universe.addEntity(entity);
-        inputSystem.addEvent(KeyCode.E, getAction());
-    }
+	private void setUp() {
+		IEntity entity = new Entity("Ben");
+		Position position = new Position();
+		entity.forceAddComponent(position, true);
+		universe.addEntity(entity);
+		inputSystem.addEvent(KeyCode.E, getAction());
+	}
 
-    private Action getAction() {
-        String script = null;
-        try {
-            script = Files.toString(new File(MOVE_SCRIPT), Charsets.UTF_8);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return new Action(script);
-    }
+	private Action getAction() {
+		String script = null;
+		try {
+			script = Files.toString(new File(MOVE_SCRIPT), Charsets.UTF_8);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return new Action(script);
+	}
 
-    @Override
-    public void start(Stage primaryStage)  {
-        primaryStage.setScene(mYInit());
-        primaryStage.show();
-        setUp();
-    }
+	@Override
+	public void start(Stage primaryStage) {
+		primaryStage.setScene(mYInit());
+		primaryStage.show();
+		setUp();
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
 
 }
