bd73fb82afd0115e80b7c2b1823212a9c2b42da7
Tom Wu <cw223@duke.edu>
2016-04-09 20:39:34 -0400

    fixed bug that crashes when spec is not specified and addComponent is called

--- src/testing/BenTestCharacter.groovy
+++ src/testing/BenTestCharacter.groovy
@@ -1,25 +1,25 @@
 package testing
 
-import api.IEntity
 import model.component.character.Health
 import model.component.character.Score
 import model.component.movement.Position
 import model.component.movement.Velocity
 import model.component.visual.ImagePath
 import model.entity.Entity
+import api.IEntity
 
 /**
  * Created by Tom on 4/7/2016.
  */
 class BenTestCharacter {
-    IEntity run(String IMAGE_PATH) {
-        IEntity character = new Entity()
-        character.forceAddComponent(new Health((double) 100), true)
-        character.forceAddComponent(new Score((double) 100), true)
-        Position pos = new Position(250.0, 250.0)
-        character.forceAddComponent(pos, true)
-        character.forceAddComponent(new ImagePath(IMAGE_PATH), true)
-        character.forceAddComponent(new Velocity(10.0, 10.0), true)
-        return character
-    }
+	IEntity run(String IMAGE_PATH) {
+		IEntity character = new Entity()
+		character.addComponent(new Health((double) 100))
+		character.addComponent(new Score((double) 100))
+		Position pos = new Position(250.0, 250.0)
+		character.addComponent(pos)
+		character.addComponent(new ImagePath(IMAGE_PATH))
+		character.addComponent(new Velocity(10.0, 10.0))
+		return character
+	}
 }
