3ccf359670982fdbac7f50832a8d22946cac6994
calisnelson <can18@duke.edu>
2016-04-11 19:18:31 -0400

    refactors

---
+++ src/main/AuthoringStartUp.java
@@ -0,0 +1,103 @@
+package main;
+
+import java.util.ResourceBundle;
+import enums.GUISize;
+import javafx.geometry.Pos;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import view.Authoring;
+import view.Utilities;
+
+public class AuthoringStartUp {
+
+	private Stage myStage;
+	private VBox myVBox;
+	private Group root;
+	private Authoring authEnv;
+	private Scene myScene;
+	private ResourceBundle myResources;
+	private ComboBox<String> games;
+	private String language;
+
+	public AuthoringStartUp(Stage stage, String language) {
+		myStage = stage;
+		this.language=language;
+		myResources = ResourceBundle.getBundle(language);
+	}
+
+	public void init() {
+		myScene = new Scene(createDisplay(), GUISize.MAIN_SIZE.getSize(), GUISize.MAIN_SIZE.getSize());
+		myStage.setScene(myScene);
+		myStage.show();
+	}
+
+	private Group createDisplay() {
+		root = new Group();
+		setVBox();
+		createGame();
+		loadGame();
+		return root;
+	}
+	
+	private void loadGame(){
+		HBox container = new HBox(GUISize.ORIG_MENU_PADDING.getSize());
+		container.setAlignment(Pos.CENTER);
+		games = Utilities.makeComboBox(myResources.getString("chooseGame"), Utilities.getAllFromDirectory("resources/createdGames"), null);
+		Button loadGame = Utilities.makeButton(myResources.getString("loadGame"), e->createAuthoringFromFile());
+		container.getChildren().addAll(games, loadGame);
+		myVBox.getChildren().add(container);
+	}
+
+	private void createGame() {
+		Button createGame = Utilities.makeButton(myResources.getString("createGame"), null);
+		createGame.setOnAction(e -> createAuthoring());
+		myVBox.getChildren().add(createGame);
+	}
+
+	private void setVBox() {
+		myVBox = new VBox(GUISize.ORIG_MENU_PADDING.getSize());
+		myVBox.prefHeightProperty().bind(myStage.heightProperty());
+		myVBox.prefWidthProperty().bind(myStage.widthProperty());
+		myVBox.setAlignment(Pos.CENTER);
+		root.getChildren().add(myVBox);
+	}
+
+	private void createAuthoring() {
+		setUpAuthoring();
+		authEnv = new Authoring(language);
+		showAuthoring();
+	}
+	
+	private void createAuthoringFromFile() {
+		setUpAuthoring();
+		String fileName = games.getSelectionModel().getSelectedItem();
+		if(fileName == null){
+			createAuthoring();
+		}
+		authEnv = new Authoring(language, fileName);
+		showAuthoring();
+		
+		
+	}
+	
+	private void setUpAuthoring() {
+		myStage.hide();
+		myStage.setWidth(GUISize.AUTHORING_WIDTH.getSize());
+		myStage.setHeight(GUISize.AUTHORING_HEIGHT.getSize());
+
+	}
+	
+	private void showAuthoring() {
+		myScene = authEnv.init(myStage.widthProperty(), myStage.heightProperty());
+		myStage.setScene(myScene);
+		myStage.show();
+	}
+
+
+
+}
--- src/main/Vooga.java
+++ src/main/Vooga.java
@@ -2,63 +2,78 @@ package main;
 
 import java.util.Arrays;
 import java.util.ResourceBundle;
-
 import enums.DefaultStrings;
 import enums.GUISize;
 import javafx.geometry.Pos;
 import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-import view.Authoring;
 import view.Utilities;
 
 public class Vooga {
-
+	
 	private Stage myStage;
-	private VBox myVBox;
-	private Group root;
-	private Authoring authEnv;
-	private Scene myScene;
 	private ResourceBundle myResources;
-	private ComboBox<String> languages, games;
-	/**
-	 * Constructor that takes in a stage to display the graphics.
-	 * 
-	 * @param stage
-	 */
+	private ComboBox<String> languages;
+	private VBox myVBox;
+	private Scene myScene;
+	private Group root;
 
 	public Vooga(Stage stage) {
 		myStage = stage;
 		myResources = ResourceBundle.getBundle(DefaultStrings.LANG_LOC.getDefault()+DefaultStrings.DEFAULT_LANGUAGE.getDefault());
 	}
-
-	/**
-	 * Initializes the scene which is displayed in the window.
-	 * 
-	 * @return the splash screen scene
-	 */
-
-	public Scene init() {
+	
+	public void init(){
 		myScene = new Scene(createDisplay(), GUISize.MAIN_SIZE.getSize(), GUISize.MAIN_SIZE.getSize());
-		return myScene;
+		myStage.setScene(myScene);
+		myStage.show();
 	}
-
-	public void draw(ImageView image) {
-		root.getChildren().add(image);
-	}
-
+	
 	private Group createDisplay() {
 		root = new Group();
 		setVBox();
-		createGame();
-		loadGame();
 		setLanguage();
+		createButtons();
 		return root;
+	}
+
+
+	private void setVBox() {
+		myVBox = new VBox(GUISize.ORIG_MENU_PADDING.getSize());
+		myVBox.prefHeightProperty().bind(myStage.heightProperty());
+		myVBox.prefWidthProperty().bind(myStage.widthProperty());
+		myVBox.setAlignment(Pos.CENTER);
+		root.getChildren().add(myVBox);
+		
+	}
+
+	private void createButtons() {
+		myVBox.getChildren().add(Utilities.makeButton(myResources.getString("makeGame"), e->createEditor()));
+		myVBox.getChildren().add(Utilities.makeButton(myResources.getString("playGame"), e->createPlayer()));
+	}
+
+
+
+	private void createPlayer() {
+		System.out.print("gotta do this still");
+	}
+	
+	private String getLanguage(){
+		String temp = languages.getSelectionModel().getSelectedItem();
+		if(temp == null){
+			temp = DefaultStrings.DEFAULT_LANGUAGE.getDefault();
+		}
+		return DefaultStrings.LANG_LOC.getDefault() + temp;
+
+	}
+
+	private void createEditor() {
+
+		AuthoringStartUp auth = new AuthoringStartUp(myStage, getLanguage());
+		auth.init();
 	}
 
 	private void setLanguage() {
@@ -67,66 +82,6 @@ public class Vooga {
 		
 	}
 	
-	private void loadGame(){
-		HBox container = new HBox(GUISize.ORIG_MENU_PADDING.getSize());
-		container.setAlignment(Pos.CENTER);
-		games = Utilities.makeComboBox(myResources.getString("chooseGame"), Utilities.getAllFromDirectory("resources/createdGames"), null);
-		Button loadGame = Utilities.makeButton(myResources.getString("loadGame"), e->createAuthoringFromFile());
-		container.getChildren().addAll(games, loadGame);
-		myVBox.getChildren().add(container);
-	}
-
-	private void createGame() {
-		Button createGame = Utilities.makeButton(myResources.getString("createGame"), null);
-		createGame.setOnAction(e -> createAuthoring());
-		myVBox.getChildren().add(createGame);
-	}
-
-	private void setVBox() {
-		myVBox = new VBox(GUISize.ORIG_MENU_PADDING.getSize());
-		myVBox.prefHeightProperty().bind(myStage.heightProperty());
-		myVBox.prefWidthProperty().bind(myStage.widthProperty());
-		myVBox.setAlignment(Pos.CENTER);
-		root.getChildren().add(myVBox);
-	}
-
-	private void createAuthoring() {
-		String lang = setUpAuthoring();
-		authEnv = new Authoring(lang);
-		showAuthoring();
-	}
-	
-	private void createAuthoringFromFile() {
-		String fileName = games.getSelectionModel().getSelectedItem();
-		if(fileName == null){
-			createAuthoring();
-		}
-		String lang = setUpAuthoring();
-		authEnv = new Authoring(lang, fileName);
-		showAuthoring();
-		
-		
-	}
-	
-	private String setUpAuthoring() {
-		myStage.hide();
-		myStage.setWidth(GUISize.AUTHORING_WIDTH.getSize());
-		myStage.setHeight(GUISize.AUTHORING_HEIGHT.getSize());
-		String temp = languages.getSelectionModel().getSelectedItem();
-		if(temp == null){
-			temp = DefaultStrings.DEFAULT_LANGUAGE.getDefault();
-		}
-		String lang = DefaultStrings.LANG_LOC.getDefault() + temp;
-		return lang;
-	}
 	
 	
-	private void showAuthoring() {
-		myScene = authEnv.init(myStage.widthProperty(), myStage.heightProperty());
-		myStage.setScene(myScene);
-		myStage.show();
-	}
-
-
-
 }
