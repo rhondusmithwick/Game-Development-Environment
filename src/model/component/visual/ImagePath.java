c35b51bb5a532543adbf4a47ac8ada62a1da5aff
Tom Wu <cw223@duke.edu>
2016-04-11 12:50:50 -0400

    updated ImagePath

--- src/model/component/visual/ImagePath.java
+++ src/model/component/visual/ImagePath.java
@@ -7,7 +7,6 @@ import api.IComponent;
 import javafx.beans.property.SimpleObjectProperty;
 import utility.SingleProperty;
 
-@Deprecated
 /**
  * Component to hold an imagePath.
  *
@@ -18,16 +17,48 @@ public class ImagePath implements IComponent {
 	/**
 	 * The singleProperty.
 	 */
-	private final SingleProperty<String> singleProperty;
+	private final SingleProperty<String> imagePathProperty, spritesheetPath;
+//	private final int framePointer;
+	private final double imageWidth, imageHeight, width, height, offsetX, offsetY;
+	private final boolean isAnimated;
 
+	public ImagePath() {
+		this("resources/RhonduSmithwick.JPG");
+	}
+	
 	/**
-	 * Construct with starting value.
+	 * Construct with no animation.
 	 *
-	 * @param imagePath
-	 *            starting value
+	 * @param imagePath starting value
 	 */
 	public ImagePath(String imagePath) {
-		singleProperty = new SingleProperty<>("ImagePath", imagePath);
+		this(imagePath, 0.0, 0.0,
+				"resources/RhonduSmithwick.JPG", 0.0, 0.0, 0.0, 0.0, false);
+	}
+	
+	/**
+	 * Construct with starting values.
+	 *
+	 * @param imagePath String path to image
+	 * @param imageWidth width of image
+	 * @param imageHeight height of image
+	 * @param spritesheetPath String path to spritesheet
+	 * @param width width of viewport
+	 * @param height height of viewport
+	 * @param offsetX offset in x-direction
+	 * @param offsetX offset in y-direction
+	 */
+	public ImagePath(String imagePath, double imageWidth, double imageHeight,
+			String spritesheetPath, double width, double height, double offsetX, double offsetY, boolean isAnimated) {
+		this.imagePathProperty = new SingleProperty<>("ImagePath", imagePath);
+		this.imageWidth = imageWidth;
+		this.imageHeight = imageHeight;
+		this.spritesheetPath = new SingleProperty<>("SpritesheetPath", spritesheetPath);
+		this.width = width;
+		this.height = height;
+		this.offsetX = offsetX;
+		this.offsetY = offsetY;
+		this.isAnimated = isAnimated;
 	}
 
 	/**
@@ -36,7 +67,7 @@ public class ImagePath implements IComponent {
 	 * @return impagePath string property
 	 */
 	public SimpleObjectProperty<String> imagePathProperty() {
-		return singleProperty.property1();
+		return imagePathProperty.property1();
 	}
 
 	public String getImagePath() {
