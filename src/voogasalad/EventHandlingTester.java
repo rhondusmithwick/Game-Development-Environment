98c58f3c48747ae7de7bc274cf553418ab119f78
Roxanne Baker <reb36@duke.edu>
2016-04-12 03:28:40 -0400

    Make EventHandlingTester same as in master and remove unused EventNotifier class

--- src/voogasalad/EventHandlingTester.java
+++ src/voogasalad/EventHandlingTester.java
@@ -1,8 +1,5 @@
 package voogasalad;
 
-import static org.junit.Assert.*;
-
-import java.awt.Robot;
 import java.util.ArrayList;
 import java.util.EventObject;
 import java.util.List;
@@ -13,12 +10,6 @@ import api.IComponent;
 import model.entity.Entity;
 import model.events.EventSystem;
 import model.events.MoveEntityLeft;
-import view.Editor;
-import view.EditorFactory;
-import api.IComponent;
-import javafx.event.ActionEvent;
-import javafx.scene.Group;
-import javafx.scene.Node;
 
 public class EventHandlingTester {
 
@@ -33,13 +24,7 @@ public class EventHandlingTester {
 		// then the event handler should also be equal
 		// will need to find a way to test for key events/mouse events
 		EventObject event = new EventObject(new Double(50));
-//		eventbus.registerTrigger(event, new MoveEntityLeft(entity));
-//		eventbus.handleEvent(new EventObject(new Double(50)));
-		Robot robot = new Robot();
-		Group myGroup = new Group();
-		Node myNode = new Node(myGroup);
-		
-		ActionEvent action = new ActionEvent(null, null, null);
-		InputEvent input = new InputEvent(EventType<KeyEvent> ke);
+		eventbus.registerTrigger(event, new MoveEntityLeft(entity));
+		eventbus.handleEvent(new EventObject(new Double(50)));
 	}
 }
