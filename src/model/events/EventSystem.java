836dfa7ab408e0cfb05693643e64f2a3a24d25bd
Roxanne Baker <reb36@duke.edu>
2016-04-12 03:26:05 -0400

    Make EventSystem same as in master

--- src/model/events/EventSystem.java
+++ src/model/events/EventSystem.java
@@ -7,9 +7,8 @@ import java.util.List;
 import java.util.Map;
 
 import api.IEventListener;
-import javafx.beans.property.StringProperty;
 
-// @Deprecated
+@Deprecated
 // BASED OFF OF IEventSystem
 /*
  * Links EventObjects and IEventListeners Utilized to call the relevant event
@@ -17,13 +16,13 @@ import javafx.beans.property.StringProperty;
  */
 public class EventSystem {
 
-	private Map<EventNotifier, List<IEventListener>> eventMap = new HashMap<EventNotifier, List<IEventListener>>();
-	private StringProperty message;
+	private Map<EventObject, List<IEventListener>> eventMap = new HashMap<EventObject, List<IEventListener>>();
+	private String message;
 
 	/*
 	 * Adds the IEventListener to the list of triggers for the given EventObject
 	 */
-	public void registerTrigger(EventNotifier event, IEventListener trigger) {
+	public void registerTrigger(EventObject event, IEventListener trigger) {
 		if (!eventMap.containsKey(event)) {
 			eventMap.put(event, new ArrayList<IEventListener>());
 		}
@@ -34,7 +33,7 @@ public class EventSystem {
 	 * Takes the given event and "deregisters" it so it is no longer considered
 	 * for event handling purposes
 	 */
-	public void deregisterTrigger(EventNotifier event) {
+	public void deregisterTrigger(EventObject event) {
 		eventMap.remove(event);
 	}
 
@@ -42,15 +41,14 @@ public class EventSystem {
 	 * Given an event, handles it if there exists a handler for it If no handler
 	 * is registered for the event, nothing happens
 	 */
-	public void handleEvent(EventNotifier event) {
-		
-//		if (eventMap.containsKey(event)) {
-//			for (IEventListener listener : eventMap.get(event)) {
-//				listener.handleEvent();
-//			}
-//			// NEED TO MODIFY HOW MESSAGE IS SET
-//			this.message = "Event handled";
-//		}
+	public void handleEvent(EventObject event) {
+		if (eventMap.containsKey(event)) {
+			for (IEventListener listener : eventMap.get(event)) {
+				listener.handleEvent();
+			}
+			// NEED TO MODIFY HOW MESSAGE IS SET
+			this.message = "Event handled";
+		}
 	}
 
 	/**
@@ -59,13 +57,8 @@ public class EventSystem {
 	 * 
 	 * @return last message received from event handler
 	 */
-	public StringProperty getLastMessage() {
+	public String getLastMessage() {
 		return message;
 	}
 
-	
-	public void publish(String message) {
-		
-	}
-	
 }
