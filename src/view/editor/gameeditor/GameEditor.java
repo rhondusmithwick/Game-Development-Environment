d5667cd10548721417c85c257ee877759849c5c8
calisnelson <can18@duke.edu>
2016-04-11 23:42:22 -0400

    refactoring

--- src/view/editor/gameeditor/GameEditor.java
+++ src/view/editor/gameeditor/GameEditor.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.ResourceBundle;
 import api.IDataWriter;
-import api.IEditor;
 import api.ISerializable;
 import datamanagement.XMLReader;
 import datamanagement.XMLWriter;
@@ -13,11 +12,9 @@ import enums.GUISize;
 import enums.Indexes;
 import enums.ViewInsets;
 import javafx.collections.FXCollections;
-import javafx.collections.ListChangeListener;
 import javafx.collections.ObservableList;
 import javafx.geometry.Pos;
 import javafx.scene.control.Label;
-import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
@@ -26,33 +23,36 @@ import model.entity.EntitySystem;
 import view.Authoring;
 import view.Utilities;
 import view.editor.Editor;
-import view.editor.EditorEntity;
-import view.editor.EditorEnvironment;
-import view.editor.EditorFactory;
 
 public class GameEditor extends Editor  {
 
-	private VBox pane, entities, environments;
+	private VBox pane;
 	private ResourceBundle myResources;
-	private EditorFactory editFact;
+	@SuppressWarnings("unused")
 	private Authoring authEnv;
 	private String myLanguage;
 	private ObservableList<ISerializable> masterEntityList;
 	private ObservableList<ISerializable> masterEnvironmentList;
 	private GameDetails gameDetails;
+	private EntityDisplay entDisp;
+	private EnvironmentDisplay envDisp;
 
-
+	public GameEditor(Authoring authEnv, String language, String fileName){
+		this(authEnv, language);
+		loadFile(fileName);
+	}
+	
 	public GameEditor(Authoring authEnv, String language){
 		myLanguage = language;
 		gameDetails = new GameDetails(language);
 		myResources = ResourceBundle.getBundle(language);
-		editFact = new EditorFactory();
 		this.authEnv=authEnv;
 		this.masterEntityList = FXCollections.observableArrayList();
 		this.masterEnvironmentList = FXCollections.observableArrayList();
+		entDisp = new EntityDisplay(myLanguage, masterEntityList, authEnv);
+		envDisp = new EnvironmentDisplay(myLanguage, masterEnvironmentList, masterEntityList, authEnv);
 		addShit();
 		setPane();
-		addListeners();
 	}
 
 	private void addShit() {
@@ -73,10 +73,6 @@ public class GameEditor extends Editor  {
 		pane.setAlignment(Pos.TOP_LEFT);
 	}
 	
-	public GameEditor(Authoring authEnv, String language, String fileName){
-		this(authEnv, language);
-		loadFile(fileName);
-	}
 
 	private void loadFile(String fileName) {
 		XMLReader<SaveGame> xReader  = new XMLReader<SaveGame>();
@@ -84,32 +80,8 @@ public class GameEditor extends Editor  {
 		gameDetails.setDetails(Arrays.asList(s.getName(), s.getDesc(), s.getIcon()));
 		masterEntityList = FXCollections.observableArrayList(s.getEntites());
 		masterEnvironmentList = FXCollections.observableArrayList(s.getEnvironments());
-		System.out.println(masterEnvironmentList.get(0));
 	}
 
-	
-	private void addListeners() {	
-		masterEntityList.addListener(new ListChangeListener<ISerializable>() {
-
-			@Override
-			public void onChanged(@SuppressWarnings("rawtypes") ListChangeListener.Change change) {
-			
-				updateEntities();
-			}
-		});
-		
-		masterEnvironmentList.addListener(new ListChangeListener<ISerializable>() {
-
-			@Override
-			public void onChanged(@SuppressWarnings("rawtypes") ListChangeListener.Change change) {
-				updateEnvironments();
-			}
-		});
-
-	}
-
-	@Override
-	public void loadDefaults() {}
 
 	@Override
 	public Pane getPane() {
@@ -131,56 +103,19 @@ public class GameEditor extends Editor  {
 	private VBox rightPane() {
 		VBox temp = new VBox(GUISize.GAME_EDITOR_PADDING.getSize());
 		temp.getChildren().add( new Label(myResources.getString("entities")));
-		temp.getChildren().add(createEntityList());
+		temp.getChildren().add(entDisp.init());
 		temp.getChildren().add( new Label(myResources.getString("environments")));
-		temp.getChildren().add(createEnvList());
+		temp.getChildren().add(envDisp.init());
 		return temp;
 	}
 	
 	private VBox leftPane() {
 		VBox temp = new VBox(GUISize.GAME_EDITOR_PADDING.getSize());
 		temp.getChildren().addAll(gameDetails.getElements());
-		editorButtons(temp);
-		temp.getChildren().add(Utilities.makeButton(myResources.getString("saveGame"), e->saveGame()));
+		temp.getChildren().addAll(Arrays.asList(entDisp.getButton(), envDisp.envButton(), Utilities.makeButton(myResources.getString("saveGame"), e->saveGame())));
 		return temp;
 	}
 	
-
-	private ScrollPane createEnvList() {
-		ScrollPane scroll = new ScrollPane();
-		environments = new VBox(GUISize.SCROLL_PAD.getSize());
-		updateEnvironments();
-		scroll.setContent(environments);
-		return scroll;
-	}
-
-	private void updateEnvironments() {
-		environments.getChildren().remove(environments.getChildren());
-		masterEnvironmentList.stream().forEach(e-> addEnvironmentToScroll((EntitySystem) e, environments));
-	}
-
-	private void addEnvironmentToScroll(EntitySystem eSystem, VBox container) {
-		container.getChildren().add(Utilities.makeButton(eSystem.getName(), f->createEditor(EditorEnvironment.class, eSystem, masterEnvironmentList )));
-	}
-
-	private ScrollPane createEntityList() {
-		ScrollPane scroll = new ScrollPane();
-		entities = new VBox(GUISize.SCROLL_PAD.getSize());
-		updateEntities();
-		scroll.setContent(entities);
-		return scroll;
-	}
-
-	private void updateEntities() {
-			entities.getChildren().remove(entities.getChildren());
-		masterEntityList.stream().forEach(e-> addEntityToScroll(e, entities));
-	}
-
-	private void addEntityToScroll(ISerializable entity, VBox container) {
-		container.getChildren().add(Utilities.makeButton(((Entity) entity).getName(), f->createEditor(EditorEntity.class, entity, FXCollections.observableArrayList())));
-
-	}
-
 	private void saveGame() {
 		SaveGame sGame = new SaveGame(gameDetails.getGameDetails(), new ArrayList<ISerializable>(masterEntityList), new ArrayList<ISerializable>(masterEnvironmentList));
 		IDataWriter<SaveGame> writer = new XMLWriter<>();
@@ -188,20 +123,6 @@ public class GameEditor extends Editor  {
 		writer.writeToFile(DefaultStrings.CREATE_LOC.getDefault() + name.trim()+ DefaultStrings.XML.getDefault(),sGame);
 		System.out.println("Saved");
 	}
-
-	private void editorButtons(VBox container) {
-		container.getChildren().add(Utilities.makeButton(myResources.getString(DefaultStrings.ENTITY_EDITOR_NAME.getDefault()), 
-				e->createEditor(EditorEntity.class, new Entity(), FXCollections.observableArrayList())));
-		container.getChildren().add(Utilities.makeButton(myResources.getString(DefaultStrings.ENVIRONMENT_EDITOR_NAME.getDefault()), 
-				e->createEditor(EditorEnvironment.class, new EntitySystem(), masterEnvironmentList)));
-	}
-	
-	private void createEditor(Class<?> editName, ISerializable toEdit, ObservableList<ISerializable> otherList) {
-		IEditor editor = editFact.createEditor(editName, myLanguage, toEdit, masterEntityList, otherList);
-		editor.populateLayout();
-		authEnv.createTab(editor.getPane(), editName.getSimpleName(), true);
-	}
-
 	
 	@Override
 	public void updateEditor() {
@@ -210,20 +131,9 @@ public class GameEditor extends Editor  {
 
 	@Override
 	public void addSerializable(ISerializable serialize) {}
+	@Override
+	public void loadDefaults() {}
 
 
-/*	public ObservableList<ISerializable> getMasterList() {
-		return masterEntityList;
-	}
-
-
-	public ObservableList<ISerializable> getAddToList() {
-		return masterEnvironmentList;
-	}
-
-
-	public void addToMaster(IEntity checkEntity) {
-		masterEntityList.add(checkEntity);
-	}*/
 
 }
