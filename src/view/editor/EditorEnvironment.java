90c8912649e55c82b5b80c53c6602d8c1681e24c
calisnelson <can18@duke.edu>
2016-04-13 16:54:04 -0400

    edits

--- src/view/editor/EditorEnvironment.java
+++ src/view/editor/EditorEnvironment.java
@@ -14,7 +14,6 @@ import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
-import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
@@ -161,9 +160,9 @@ public class EditorEnvironment extends Editor {
 	}
 
 	private void saveEnvironment() {
-		entitiesInEnvironment.removeAllBindingsFromComponents();
 		String name = getName();
 		entitiesInEnvironment.setName(name);
+		finalEnvironmentList.remove(entitiesInEnvironment);
 		finalEnvironmentList.add(entitiesInEnvironment);
 		environmentPane.getChildren().clear();
 		environmentPane.setCenter(saveMessage(myResources.getString("saveMessage")));
