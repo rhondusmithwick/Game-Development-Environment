fa0a5bb00bbd5b25ae0abb6b91ca4b7f51a4e6bb
Ben Zhang <bwz2@duke.edu>
2016-04-13 23:00:09 -0400

    Add music

--- src/view/beginingmenus/StartUpMenu.java
+++ src/view/beginingmenus/StartUpMenu.java
@@ -1,5 +1,6 @@
 package view.beginingmenus;
 
+import java.io.File;
 import java.util.List;
 
 import enums.GUISize;
@@ -8,16 +9,20 @@ import javafx.scene.Group;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.layout.VBox;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
 import javafx.stage.Stage;
 
 public abstract class StartUpMenu {
 
-	
 	private Scene myScene;
 	private Stage myStage;
 	private Group root;
 	private VBox myVBox;
-	
+	private Media media;
+	private MediaPlayer mediaPlayer;
+	private static final String MUSIC = "resources/music/";
+	private static final String MAIN_THEME = "finalCountdown.mp3";
 	
 	public StartUpMenu(Stage myStage){
 		this.myStage = myStage;
@@ -25,6 +30,7 @@ public abstract class StartUpMenu {
 
 	public void init() {
 		myScene = new Scene(createDisplay(), GUISize.MAIN_SIZE.getSize(), GUISize.MAIN_SIZE.getSize());
+		setMusic(MUSIC + MAIN_THEME);
 		myStage.setScene(myScene);
 		myStage.show();
 	}
@@ -33,6 +39,13 @@ public abstract class StartUpMenu {
 		root = new Group();
 		createVBox();
 		return root;
+	}
+	
+	private void setMusic(String file) {
+        media = new Media(new File(file).toURI().toString());
+        mediaPlayer = new MediaPlayer(media);
+        mediaPlayer.setCycleCount(MediaPlayer.INDEFINITE);
+        mediaPlayer.play();	
 	}
 
 	private void createVBox() {
@@ -46,5 +59,4 @@ public abstract class StartUpMenu {
 	public void addNodesToVBox(List<Node> toAdd){
 		myVBox.getChildren().addAll(toAdd);
 	}
-	
 }
