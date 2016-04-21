6b5a3bf7d8eaf83b6ffb418ceb8a52118a1e6e71
Rhondu Smithwick <rhondusmithwick@gmail.com>
2016-04-21 00:03:09 -0400

    Added save handler.

---
+++ src/animation/SaveHandler.java
@@ -0,0 +1,36 @@
+package animation;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Properties;
+
+/**
+ * Created by rhondusmithwick on 4/20/16.
+ *
+ * @author Rhondu Smithwick
+ */
+public class SaveHandler {
+
+    private SaveHandler() {
+    }
+
+    public static void save(String fileName, Map<String, Map<String, String>> maps) {
+        Properties properties = new Properties();
+        for (String animationName : maps.keySet()) {
+            Map<String, String> props = maps.get(animationName);
+            for (Entry<String, String> prop : props.entrySet()) {
+                String key = animationName + prop.getKey();
+                properties.put(key, prop.getValue());
+            }
+        }
+        try {
+            properties.store(new FileWriter(new File(fileName)), "MELISSA IS MAKING ME USE THIS WEIRD ASS MAP");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
