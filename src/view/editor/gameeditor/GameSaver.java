956a50577335e5bd3f276849c385c3bb39acc8cf
calisnelson <can18@duke.edu>
2016-04-21 17:04:01 -0400

    save other stuff

--- src/view/editor/gameeditor/GameSaver.java
+++ src/view/editor/gameeditor/GameSaver.java
@@ -1,6 +1,7 @@
 package view.editor.gameeditor;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.List;
 
 import api.IEntity;
@@ -16,12 +17,29 @@ public class GameSaver {
 	
 	@SuppressWarnings("unchecked")
 	public void saveGame(ObservableList<ILevel> levels, ObservableList<IEntity> entityList, List<String> details){
+		String fileName = getDirectory(details);
+		new XMLWriter<List<String>>().writeToFile(fileName + "/metadata.xml", details);
+		new XMLWriter<List<IEntity>>().writeToFile(fileName+"/entities.xml", new ArrayList<IEntity>(entityList));
+		saveLevels(levels, fileName);
+	}
+
+
+	private void saveLevels(ObservableList<ILevel> levels, String fileName) {
+		File levelFile = new File(fileName + "/levels");
+		levelFile.mkdirs();
+		for(ILevel level: levels){
+			new XMLWriter<ILevel>().writeToFile(fileName + "/levels/" + level.getName() + ".xml", level);
+		}
+	}
+
+	
+	private String getDirectory(List<String> details) {
 		String fileName = DefaultStrings.CREATE_LOC.getDefault() + details.get(Indexes.GAME_NAME.getIndex());
 		File file = new File(fileName);
-		if(!file.exists()){
-			file.mkdirs();
+		if(file.exists()){
+			file.delete();
 		}
-		
-		new XMLWriter<List<String>>().writeToFile(fileName + "/metadata.xml", details);
+		file.mkdirs();
+		return fileName;
 	}
 }
