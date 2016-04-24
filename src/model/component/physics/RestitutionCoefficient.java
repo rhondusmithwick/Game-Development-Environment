e5e840c3c91f822c5caa1503f618b8a5cf1d097f
Tom Wu <cw223@duke.edu>
2016-04-24 03:06:38 -0400

    about to implement explosive collisions

--- src/model/component/physics/RestitutionCoefficient.java
+++ src/model/component/physics/RestitutionCoefficient.java
@@ -1,12 +1,11 @@
 package model.component.physics;
 
-import api.IComponent;
-import com.google.common.base.Preconditions;
-import javafx.beans.property.SimpleObjectProperty;
-import utility.SingleProperty;
-
 import java.util.Collections;
 import java.util.List;
+
+import api.IComponent;
+import javafx.beans.property.SimpleObjectProperty;
+import utility.SingleProperty;
 
 /**
  *
@@ -14,32 +13,32 @@ import java.util.List;
  */
 public class RestitutionCoefficient implements IComponent {
 
-    private final SingleProperty<Double> singleProperty = new SingleProperty<>("CoefficientofRestitution", 0.0);
+	private final SingleProperty<Double> singleProperty = new SingleProperty<>("CoefficientofRestitution", 0.0);
 
-    public RestitutionCoefficient() {
-    }
+	public RestitutionCoefficient() {
+	}
 
-    public RestitutionCoefficient(double restitutionCoefficient) {
-        setRestitutionCoefficient(restitutionCoefficient);
-    }
+	public RestitutionCoefficient(double restitutionCoefficient) {
+		setRestitutionCoefficient(restitutionCoefficient);
+	}
 
-    public SimpleObjectProperty<Double> restitutionCoefficientProperty() {
-        return singleProperty.property1();
-    }
+	public SimpleObjectProperty<Double> restitutionCoefficientProperty() {
+		return singleProperty.property1();
+	}
 
-    public double getRestitutionCoefficient() {
-        return restitutionCoefficientProperty().get();
-    }
+	public double getRestitutionCoefficient() {
+		return restitutionCoefficientProperty().get();
+	}
 
-    public void setRestitutionCoefficient(double restitutionCoefficient) {
-        boolean valid = (restitutionCoefficient >= 0) && (restitutionCoefficient <= 1);
-        Preconditions.checkArgument(valid, "Coefficient of restitution should be between 0 and 1");
-        restitutionCoefficientProperty().set(restitutionCoefficient);
-    }
+	public void setRestitutionCoefficient(double restitutionCoefficient) {
+		boolean valid = (restitutionCoefficient >= 0) && (restitutionCoefficient <= 1);
+		// Preconditions.checkArgument(valid, "Coefficient of restitution should
+		// be between 0 and 1");
+		restitutionCoefficientProperty().set(restitutionCoefficient);
+	}
 
-
-    @Override
-    public List<SimpleObjectProperty<?>> getProperties() {
-        return Collections.singletonList(restitutionCoefficientProperty());
-    }
+	@Override
+	public List<SimpleObjectProperty<?>> getProperties() {
+		return Collections.singletonList(restitutionCoefficientProperty());
+	}
 }
