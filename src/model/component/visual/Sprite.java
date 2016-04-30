9c35734ecbd2ee4d6479710e967af6f4ab386dd5
Tom Wu <cw223@duke.edu>
2016-04-29 23:49:20 -0400

    Fixed pong

--- src/model/component/visual/Sprite.java
+++ src/model/component/visual/Sprite.java
@@ -22,9 +22,10 @@ import java.util.List;
 public class Sprite implements IComponent {
 
     private static final String DEFAULT_IMAGE_PATH = "resources/testing/RhonduSmithwick.JPG";
+    private static final double DEFAULT_WIDTH = 638.0, DEFAULT_HEIGHT = 518.0;
 
     private final SingleProperty<String> imagePathProperty = new SingleProperty<>("ImagePath", DEFAULT_IMAGE_PATH);
-    private final TwoProperty<Double, Double> imageSizeProperty = new TwoProperty<>("ImageWidth", 0.0, "ImageHeight", 0.0);
+    private final TwoProperty<Double, Double> imageSizeProperty = new TwoProperty<>("ImageWidth", DEFAULT_WIDTH, "ImageHeight", DEFAULT_HEIGHT);
     private final SingleProperty<Integer> zLevelProperty = new SingleProperty<>("zLevel", 0);
     private transient ImageView imageView;
 
@@ -37,12 +38,12 @@ public class Sprite implements IComponent {
      *
      * @param imagePath starting value
      */
-    public Sprite (String imagePath) { // TODO: place default in resource file
+    public Sprite (String imagePath) {
         setImagePath(imagePath);
 //        Image image = getImage(imagePath);
 //        setImageWidth(image.getWidth());
 //        setImageHeight(image.getHeight());
-        imageView = createImageView(imagePath);
+//        imageView = createImageView(imagePath); // TODO: remove?
     }
 
     /**
