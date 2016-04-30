bafb348dcef8a1f22ac94c8c434ba36e038adaa1
Melissa Zhang <mfz2@duke.edu>
2016-04-30 12:46:12 -0400

    groovy punch

--- src/testing/demo/MoveAndSound.groovy
+++ src/testing/demo/MoveAndSound.groovy
@@ -1,3 +1,5 @@
+package testing.demo
+
 import events.Action
 import events.KeyTrigger
 import groovy.lang.GroovyShell;
@@ -7,22 +9,30 @@ import api.IGameScript
 import api.ILevel;
 import api.IPhysicsEngine;
 import api.ISystemManager;
+import api.*
+import model.component.movement.Position
 
-public class punch implements IGameScript{
+
+
+public class MoveAndSound implements IGameScript{
 	
 	private ISystemManager game;
 	private ILevel universe;
 	private final IPhysicsEngine physics = new PhysicsEngine();
+	public static final String PATH = "src/testing/demo/";
+	
+	private final String punchScript = PATH + "Punch.groovy";
+	
 	private IEventSystem events;
 	@Override
 	public void init(GroovyShell shell, ISystemManager game) {
 		this.game = game;
 		this.universe = game.getEntitySystem();
 		this.events = universe.getEventSystem()
-		shell.setVariable("punch");
+		shell.setVariable("punchandsound");
 		initKeyInputs();
 		initSprites();
-		}
+				}
 
 	@Override
 	public void update(double dt) {
@@ -33,10 +43,16 @@ public class punch implements IGameScript{
 
 		Map<String, Object> sKey = new HashMap<>();
 		sKey.put("key", "S");
-
-		events.registerEvent(new KeyTrigger("S"), new Action(movePaddleScript, sKey));
-	}
+		Map<String, Object> wKey = new HashMap<>();
+		wKey.put("key", "W");
+		events.registerEvent(new KeyTrigger("W"), new Action(punchScript, wKey));
+		
+		events.registerEvent(new KeyTrigger("S"), new Action(punchScript, sKey));
+	
+		}
     private void initSprites() {
+		IEntity sprite = SpriteLoader.createAnimatedSprite("ryu",new Position(50.0, 150.0));
+		universe.addEntities(sprite);
     }
 		
 }
