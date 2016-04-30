03bf70ae70e2e8a3833836ced61c2a1287351b0d
Rhondu Smithwick <rhondusmithwick@gmail.com>
2016-04-30 16:35:01 -0400

    Added some more groovy things1

--- resources/groovyScripts/AnimationScript.groovy
+++ resources/groovyScripts/AnimationScript.groovy
@@ -1,41 +1,23 @@
 package groovyScripts
 
 import api.IEntity
-import api.ILevel
-import groovy.transform.Field
+import groovy.transform.BaseScript
 import model.component.visual.AnimatedSprite
+@BaseScript ScriptHelpers ScriptHelpers
 
 /**
  * Created by rhondusmithwick on 4/30/16.
  * @author Rhondu Smithwick
  */
-@Field ILevel universe = (ILevel) binding.getVariable("universe");
 
 void animate(IEntity entity) {
     if (entity.hasComponent(AnimatedSprite.class)) {
         AnimatedSprite animatedSprite = entity.getComponent(AnimatedSprite.class);
-        String animationName = (String) binding.getVariable("animationName");
+        String animationName = (String) getVariable("animationName");
         animatedSprite.createAndPlayAnimation(animationName);
     }
 }
 
-void runAllEntities() {
-    List<IEntity> entities = new ArrayList<>();
-    if (containsVariable("entityID")) {
-        String entityID = (String) binding.getVariable("entityID");
-        entities.add(universe.getEntity(entityID));
-    }
-    if (containsVariable("entityName")) {
-        String entityName = (String) binding.getVariable("entityName");
-        entities.addAll(universe.getEntitiesWithName(entityName));
-    }
-    for (IEntity entity : entities) {
-        animate(entity);
-    }
+for (IEntity entity: getEntitieWithNamesAndIDs()) {
+    animate(entity);
 }
-
-boolean containsVariable (String variableName) {
-    return binding.variables.containsKey(variableName)
-}
-
-runAllEntities()
---
+++ resources/groovyScripts/ScriptHelpers.groovy
@@ -0,0 +1,34 @@
+package groovyScripts
+
+import api.IEntity
+import api.ILevel
+import groovy.transform.Field
+
+import java.util.function.Consumer
+
+/**
+ * Created by rhondusmithwick on 4/30/16.
+ * @author Rhondu Smithwick
+ */
+@Field ILevel universe = (ILevel) binding.getVariable("universe");
+
+boolean containsVariable (String variableName) {
+    return binding.variables.containsKey(variableName)
+}
+
+Object getVariable(String variableName) {
+    return binding.getVariable(variableName);
+}
+
+List<IEntity> getEntitieWithNamesAndIDs() {
+    List<IEntity> entities = new ArrayList<>();
+    if (containsVariable("entityID")) {
+        String entityID = (String) getVariable("entityID");
+        entities.add(universe.getEntity(entityID));
+    }
+    if (containsVariable("entityName")) {
+        String entityName = (String) getVariable("entityName");
+        entities.addAll(universe.getEntitiesWithName(entityName));
+    }
+    return entities;
+}
