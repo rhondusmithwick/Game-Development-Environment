0e84c9915d0edb69d493782018b0de0a6724519f
Rhondu Smithwick <rhondusmithwick@gmail.com>
2016-04-30 17:50:10 -0400

    Added more scripts.

--- resources/groovyScripts/MoveEntity.groovy
+++ resources/groovyScripts/MoveEntity.groovy
@@ -3,24 +3,22 @@ package groovyScripts
 import api.IEntity
 import groovy.transform.BaseScript
 import groovy.transform.Field
+import model.component.movement.Velocity
 
 @BaseScript ScriptHelpers ScriptHelpers
 
 /**
- * Move an Entity by the provided X and Y.
+ * Created by rhondusmithwick on 4/30/16.
  * @author Rhondu Smithwick
  */
 
-import model.component.movement.Position;
 
-@Field Double movedX = containsVariable("movedX") ? (Double) getVariable("movedX") : 0.0;
-@Field Double movedY = containsVariable("movedY") ? (Double) getVariable("movedY") : 0.0;
-
+@Field Double movedX = containsVariable("velocityX") ? (Double) getVariable("velocityX") : 0.0;
+@Field Double movedY = containsVariable("velocityY") ? (Double) getVariable("velocityY") : 0.0;
 
 void move(IEntity entity) {
-    Position position = entity.getComponent(Position.class);
-    position.setX(position.getX() + movedX);
-    position.setY(position.getY() + movedY);
+    Velocity velocity = entity.getComponent(Velocity.class);
+    velocity.setVXY(movedX, movedY);
 }
 
 for (IEntity entity: getEntitiesWithNamesAndIDs()) {
