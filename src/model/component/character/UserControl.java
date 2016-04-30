89c35ce4debc6e773736c0b2658980b397d91b89
Anirudh Jonnavithula <jonnavithula.anirudh@gmail.com>
2016-04-30 13:16:15 -0400

    pushing before pulling from master

---
+++ src/model/component/character/UserControl.java
@@ -0,0 +1,13 @@
+package model.component.character;
+
+import api.IComponent;
+
+public class UserControl implements IComponent{
+
+	@Override
+	public void update() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
