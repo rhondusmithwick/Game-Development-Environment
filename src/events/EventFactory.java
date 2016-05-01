5b671500c89c0ca05a4cc4a694dd365d1722532e
Rhondu Smithwick <rhondusmithwick@gmail.com>
2016-05-01 12:59:20 -0400

    Made EventFactory a Utility.

--- src/events/EventFactory.java
+++ src/events/EventFactory.java
@@ -3,6 +3,7 @@ package events;
 import utility.Pair;
 import voogasalad.util.reflection.Reflection;
 
+import java.awt.*;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -13,19 +14,22 @@ import java.util.Map;
 
 public final class EventFactory {
 
-    public Pair<Trigger, Action> createEvent (String triggerName, String scriptPath,
+    private EventFactory() {
+    }
+    
+    public static Pair<Trigger, Action> createEvent (String triggerName, String scriptPath,
                                               Map<String, Object> parameters, Object... args) {
         Trigger trigger = createTrigger(triggerName, args);
         Action action = new Action(scriptPath, parameters);
         return new Pair<>(trigger, action);
     }
 
-    public Pair<Trigger, Action> createEvent (String triggerName, String scriptPath,
+    public static  Pair<Trigger, Action> createEvent (String triggerName, String scriptPath,
                                               Object... args) {
-        return createEvent(triggerName, scriptPath, new HashMap<String, String>(), args);
+        return createEvent(triggerName, scriptPath, new HashMap<>(), args);
     }
 
-    public Trigger createTrigger (String className, Object... args) {
+    public static Trigger createTrigger (String className, Object... args) {
         String eventsDirectoryPath = "events.";
         return (Trigger) Reflection.createInstance(eventsDirectoryPath + className, args);
     }
