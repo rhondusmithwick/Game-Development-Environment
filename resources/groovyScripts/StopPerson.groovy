a601ceec573331235b1782c49e4fc2c20e6ed843
Rhondu Smithwick <rhondusmithwick@gmail.com>
2016-04-30 20:44:16 -0400

    Added Stop Person Again.

---
+++ resources/groovyScripts/StopPerson.groovy
@@ -0,0 +1,21 @@
+package groovyScripts
+
+import api.IEntity
+import groovy.transform.BaseScript
+import model.component.movement.Velocity
+
+/**
+ * Created by ajonnav on 4/27/16.
+ * @author Anirudh Jonnavithula
+ */
+
+@BaseScript ScriptHelpers ScriptHelpers
+
+void resetVelocity (IEntity entity) {
+    Velocity velocity = entity.getComponent(Velocity.class);
+    velocity.setVXY(0, 0);
+}
+
+for (IEntity entity: getEntitiesWithNamesAndIDs()) {
+    resetVelocity(entity);
+}
