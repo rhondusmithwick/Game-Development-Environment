709ec43ab90bd5d97d389bad35d21ff12a553fa6
Anirudh Jonnavithula <jonnavithula.anirudh@gmail.com>
2016-05-01 21:34:01 -0400

    Testing things

--- src/testing/games/ACGameMain.java
+++ src/testing/games/ACGameMain.java
@@ -22,7 +22,7 @@ public class ACGameMain extends Application {
     public static final int FRAMES_PER_SECOND = 60;
     private static final int MILLISECOND_DELAY = 1000 / FRAMES_PER_SECOND;
     private static final double SECOND_DELAY = 1.0 / FRAMES_PER_SECOND;
-    private ACGame myGame;
+    private LevelChangeTest myGame;
 
     public static void main (String[] args) {
 
@@ -34,7 +34,7 @@ public class ACGameMain extends Application {
      */
     public void start (Stage s) {
         // create your own game here
-        myGame = new ACGame();
+        myGame = new LevelChangeTest();
         s.setTitle(myGame.getTitle());
         Scene scene = myGame.init(SIZE, SIZE);
         s.setScene(scene);
