aabb660ddbf26662edddf3f0b9a28d56115ed771
calisnelson <can18@duke.edu>
2016-05-06 11:28:37 -0400

    masterpiece

--- src/view/componentdisplays/ComponentDisplayIntegerSlider.java
+++ src/view/componentdisplays/ComponentDisplayIntegerSlider.java
@@ -1,3 +1,20 @@
+// This entire file is part of my masterpiece.
+// Cali Nelson
+
+/**
+ * This class is an example of a subclass of the componentDisplaySlider. As you can see it only took about 20 lines 
+ * of code to add this type of slider in because the abstract class handles most of work, and the only thing needed
+ * from this class is creating the slider and getting its value so that both work with an integer property. Another good 
+ * design feature is that the values for setting the sliders minimum and maximum values, and the major and minor
+ * tick intervals are contained in properties files
+ * so that they can easily be customised for each property. So for example if you wanted the minimum for the velocity property
+ * to be -100.0 but the minimum for the mass property to be 1, all you would have to do is modify the values in the 
+ * guiObject/guiComponents.properties files. Also if you wanted to allow the user only to set the value of the lives property
+ * in multiples of ten, but allow them to set another property in increments of 1, all you would have to do is set different 
+ * minor increment values in the properties files. Thus these customization options are not hardcoded in, so each property can
+ * be customized in the way that makes the most sense for that property and for the user.
+ */
+
 package view.componentdisplays;
 
 import javafx.beans.property.SimpleObjectProperty;
--- src/view/componentdisplays/GuiObjectDoubleSlider.java
+++ src/view/componentdisplays/GuiObjectDoubleSlider.java
@@ -1,3 +1,17 @@
+// This entire file is part of my masterpiece.
+// Cali Nelson
+
+/**
+ * This class is an example of a subclass of the componentDisplaySlider. As you can see it only took about 15 lines 
+ * of code to add this type of slider in because the abstract class handles most of work, and the only thing needed
+ * from this class is creating the slider and getting its value so that both work with a double property. Another good 
+ * design feature is that the values for setting the sliders minimum and maximum values are contained in properties files
+ * so that they can easily be customised for each property. So for example if you wanted the minimum for the velocity property
+ * to be -100.0 but the minimum for the mass property to be 1, all you would have to do is modify the values in the 
+ * guiObject/guiComponents.properties files. Thus these maxes and mins are not hardcoded in, so there are avenues to 
+ * customize each slider to the property it represents.
+ */
+
 package view.componentdisplays;
 
 import javafx.beans.property.SimpleObjectProperty;
